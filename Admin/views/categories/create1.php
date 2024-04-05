0680d479e2f44f390dc814a901eee579fc2dfaf8
hao <haotienphung@gmail.com>
2024-04-05 20:18:03 +0700

    hao

--- Admin/views/categories/create1.php
+++ Admin/views/categories/create1.php
@@ -1,8 +1,3 @@
-
-<script></script>
-<style>
-
-</style>
 <h2>Thêm mới dịch vụ</h2>
 <form method="post" action="" enctype="multipart/form-data">
     <div class="form-group">
@@ -21,31 +16,11 @@
                   name="content"><?php echo isset($_POST['content']) ? $_POST['content'] : ''; ?></textarea>
     </div>
     <div class="form-group">
-            <label style="color: black">Mô tả chi tiết về dịch vụ</label>
-            <textarea class="form-control"
-                      name="comment"><?php echo isset($_POST['comment']) ? $_POST['comment'] : ''; ?></textarea>
-        </div>
-<!--    <div class="form-group">-->
-<!--      --><?php
-//      $selected_active = '';
-//      $selected_disabled = '';
-//      if (isset($_POST['status'])) {
-//        switch ($_POST['status']) {
-//          case 0:
-//            $selected_disabled = 'selected';
-//            break;
-//          case 1:
-//            $selected_active = 'selected';
-//            break;
-//        }
-//      }
-//      ?>
-<!--        <label>Trạng thái</label>-->
-<!--        <select name="status" class="form-control">-->
-<!--            <option value="0" --><?php //echo $selected_disabled ?><!-- >Active</option>-->
-<!--            <option value="1" --><?php //echo $selected_active ?><!-- >Disabled</option>-->
-<!--        </select>-->
-<!--    </div>-->
+        <label style="color: black">Mô tả chi tiết về dịch vụ</label>
+        <textarea class="form-control"
+                  name="comment"><?php echo isset($_POST['comment']) ? $_POST['comment'] : ''; ?></textarea>
+    </div>
+
 
     <input type="submit" class="btn btn-primary" name="submit" value="Save"/>
     <input type="reset" class="btn btn-secondary" name="submit" value="Reset"/>
