f8961ea2d8ef2d9c2135eb19703f18d08fecaf8e
hao <haotienphung@gmail.com>
2024-04-18 18:55:53 +0700

    hao

--- User/views/homes/index.php
+++ User/views/homes/index.php
@@ -55,6 +55,118 @@
         <button class="l1"><a href="index.php?controller=home&action=aboutus"> Tìm Hiểu Thêm</a></button>
     </div>
 </div>
+    <div class="bac">
+        <h5 class="kol"> Các sản phẩm DTSVN mang lại đều đang giải quyết đúng được nhu cầu thị trường,<br> đã được cấp chứng chỉ sở hữu trí tuệ của Nhà nước, sẵn sàng đồng hành cùng tổ chức tín dụng lớn,<br> vừa và nhỏ phát triển một cách tổng thế và bền vững.</h5>
+        <div class="f-grid">
+            <div class="f-grid-col">
+                <img src="image/a9.jpg" width="230" height="250">
+                <h4 class="tt">Mục tiêu</h4>
+                <p class="t">Luôn sẵn sàng tạo ra những sản phẩm, dịch vụ mang lại hiệu quả nhất cho hoạt động kinh doanh của khách hàng. Tạo ra sự khác biệt với các đối thủ cạnh tranh bằng chất lượng và cam kết về sản phẩm dịch vụ.</p>
+
+            </div>
+            <div class="f-grid-col">
+                <img src="image/a10.jpg" width="230" height="250">
+                <h4 class="tt">Tầm nhìn</h4>
+                <p class="t">Hoạt động dựa trên Hệ giá trị cốt lõi (CRV). C – Customer Centric : Khách hàng là trọng tâm. R - Responsibility: Trách nhiệm mang lại hiệu quả. V - Values: Giá trị đích thực (Sản phẩm, dịch vụ mang lại sự khác biệt và giá trị cho khách hàng).</p>
+
+            </div>
+            <div class="f-grid-col">
+                <img src="image/a11.jpg" width="230" height="250">
+                <h4 class="tt">Sứ mệnh</h4>
+                <p class="t">Trở thành công ty có thế mạnh khác biệt vê sản phẩm, dịch vụ trong khối Tài chính Ngân hàng. Nâng cao năng lực cốt lõi, trách nhiệm và sáng tạo.</p>
+            </div>
+            <div class="f-grid-col">
+                <img src="image/a12.jpg" width="230" height="250">
+                <h4 class="tt">Cam kết</h4>
+                <p class="t">Cung cấp sản phầm phần mềm đem lại giải pháp toàn diện cho các tổ chức tín dụng: hợp lý hóa quy trình làm việc và tăng năng suất công việc. Hóa giải nhiều trở ngại, tiết kiệm chi phí, lợi nhuận tăng cao.</p>
+            </div>
+            <div class="f-grid-col">
+                <img src="image/a13.jpg" width="230" height="250">
+                <h4 class="tt">Hiệu xuất</h4>
+                <p class="t">Chúng tôi cung cấp giải pháp phần mềm chuyển đổi số đi kèm môi trường làm việc nhóm tương tác trên không gian làm việc số: nơi mà mọi người dùng hệ thống được cộng tác – gắn kết & phát triển.
+
+                    Cty giúp bạn nâng cao năng suất làm việc trong toàn tổ chức, nơi mọi nhân viên luôn mỉm cười, tự hào và hạnh phúc.</p>
+            </div>
+        </div>
+    </div>
+    <!--xếp hạng-->
+    <div class="mainnn">
+        <div class="main-skills">
+            <div class="container center">
+                <div class="title-skills">
+                    <h3>CÔNG TY TNHH ĐÀO TẠO VÀ CHUYỂN GIAO CÔNG NGHỆ SỐ VN :</h3>
+                    <div class="title-line"></div>
+                    <p>Với đội ngũ chuyên gia sáng tạo và nhiệt huyết, chúng tôi luôn tiên phong mang lại những sản phẩm <br>hỗ trợ doanh nghiệp chuyển đổi số độc đáo, hữu ích với giá thành cạnh tranh cho khách hàng.</p>
+                </div>
+                <div class="content-skills">
+                    <div class=" card col-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="box">
+                            <div>
+                                <div class="percent">
+                                    <svg>
+                                        <circle cx="70" cy="70" r="70"></circle>
+                                        <circle cx="70" cy="70" r="70"></circle>
+                                    </svg>
+                                    <div class="number">
+                                        <h2>90<span>%</span></h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text">Kinh nghiệm</div>
+                    </div>
+                    <div class=" card col-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="box">
+                            <div>
+                                <div class="percent">
+                                    <svg>
+                                        <circle cx="70" cy="70" r="70"></circle>
+                                        <circle cx="70" cy="70" r="70"></circle>
+                                    </svg>
+                                    <div class="number">
+                                        <h2>85<span>%</span></h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text">Hệ thống</div>
+                    </div>
+                    <div class=" card col-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="box">
+                            <div>
+                                <div class="percent">
+                                    <svg>
+                                        <circle cx="70" cy="70" r="70"></circle>
+                                        <circle cx="70" cy="70" r="70"></circle>
+                                    </svg>
+                                    <div class="number">
+                                        <h2>80<span>%</span></h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text">Dự án</div>
+                    </div>
+                    <div class=" card col-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="box">
+                            <div>
+                                <div class="percent">
+                                    <svg>
+                                        <circle cx="70" cy="70" r="70"></circle>
+                                        <circle cx="70" cy="70" r="70"></circle>
+                                    </svg>
+                                    <div class="number">
+                                        <h2>95<span>%</span></h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text">Khách hàng</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
@@ -106,6 +218,152 @@
         cursor: pointer;
         font-size: 20px;
         transition: all 0.5s ease;
+    }
+    .bac{
+        background: #efebeb;
+    }
+    .kol{
+        padding: 100px 20px 30px;
+        text-align: center;
+    }
+
+    .f-grid {
+        margin-bottom: 100px;
+        display: flex;
+        justify-content: space-between;
+        margin-left: -1rem;
+        flex-flow: row wrap;
+    }
+
+    .f-grid-col {
+        margin: 0px 0px 100px 0px;
+        text-align: center;
+        flex: 1 0;
+        /*margin-left: 1rem;
+        margin-bottom: 1rem;*/
+        padding: 2rem;
+        color: white;
+    }
+    .t{
+        color: black;
+        text-align: justify;
+    }
+    .tt{
+        color: red;
+        text-align: center;
+    }
+    /* xếp hạng sp                            */
+    .mainnn{
+
+
+    }
+
+    .main-skills {
+        padding: 50px 0 200px;
+        color: #FFFFFF;
+        text-align: center;
+    }
+    .title-skills h3 {
+        color: #3B4660;
+    }
+    .title-skills .title-line {
+        background-color: #DDE7EC;
+    }
+    .title-skills p {
+        color: #646B7F;
+        margin-top: 5px;
+    }
+    .content-skills {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+    .content-skills .card {
+        position: relative;
+        height: 200px;
+        text-align: center;
+        z-index: 10;
+        border: none;
+    }
+    .content-skills .card .box {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .content-skills .percent {
+        position: relative;
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        z-index: 100;
+    }
+    .content-skills .percent .number {
+        position: absolute;
+        top: 0;
+        left: 5px;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .content-skills .percent .number h2 {
+        color: #303C57;
+        font-weight: 300;
+        font-size: 45px;
+    }
+    .content-skills .percent .number h2 span {
+        font-size: 24px;
+    }
+    .content-skills .text {
+        background: white;
+        color: black;
+        position: absolute;
+        bottom: -20px;
+        left: 50%;
+        transform: translate(-50%);
+        font-weight: 700;
+        font-size: 18px;
+        width: 100%;
+    }
+    .content-skills svg {
+        position: relative;
+        width: 150px;
+        height: 150px;
+        transform: rotate(-90deg);
+    }
+    .content-skills svg circle {
+        width: 100%;
+        height: 100%;
+        fill: none;
+        stroke-width: 10;
+        stroke: #DEE8ED;
+        transform: translate(5px, 5px);
+    }
+    .content-skills svg circle:nth-child(2) {
+        stroke-dasharray: 440px;
+        stroke-dashoffset: 80px;
+    }
+    .content-skills .card:nth-child(1) svg circle:nth-child(2) {
+        stroke: #30BAE7;
+        stroke-dashoffset: calc(440px - (440px * 90)/100);
+    }
+    .content-skills .card:nth-child(2) svg circle:nth-child(2) {
+        stroke: #D74680;
+        stroke-dashoffset: calc(440px - (440px * 75)/100);
+    }
+    .content-skills .card:nth-child(3) svg circle:nth-child(2) {
+        stroke: #15C7A8;
+        stroke-dashoffset: calc(440px - (440px * 70)/100);
+    }
+    .content-skills .card:nth-child(4) svg circle:nth-child(2) {
+        stroke: #EB7D4B;
+        stroke-dashoffset: calc(440px - (440px * 85)/100);
     }
 </style>
 
